15e5efeaa6d297922e7dc9a8b38b2606c02aba87
parabatareek <gavrosh07@gmail.com>
2022-09-26 20:27:27 +0300

    change: *Упражнение 2

--- Sprint1/4.gotest/gotest_2/gotest_2_test.go
+++ Sprint1/4.gotest/gotest_2/gotest_2_test.go
@@ -7,7 +7,7 @@ type fields struct {
 	LastName  string
 }
 
-func TestFullName(t *testing.T) {
+func TestUser_FullName(t *testing.T) {
 	tests := []struct {
 		name   string
 		fields fields
@@ -31,7 +31,13 @@ func TestFullName(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			if got := Full
+			u := User{
+				FirstName: test.fields.FirstName,
+				LastName:  test.fields.LastName,
+			}
+			if got := u.FullName(); got != test.want {
+				t.Errorf("Result:%v. Want: %v", got, test.want)
+			}
 		})
 	}
 }
